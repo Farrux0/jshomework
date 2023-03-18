17168341e26025d91479e974c4b416b7dea15f73
Farrux <fkgmc@.ru>
2023-03-18 23:41:52 +0000

    ver1

---
+++ main.js
@@ -0,0 +1,47 @@
+let man = {
+    1: {
+        men1: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    2: {
+        men2: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    3: {
+        men3: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    4: {
+        men4: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    5: {
+        men5: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    6: {
+        men6: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    7: {
+        men7: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    8: {
+        men8: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    9: {
+        men9: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    },
+    10: {
+        men10: prompt("Введите имя"),
+        age: +prompt("Введите возраст")
+    }
+ 
+
+}
+for(let key in man){
+    console.log(man[key]);
+}
